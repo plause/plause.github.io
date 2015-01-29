44f4c2525cb9d4f3753355f5cc54869e8c7def88
Huazhou Wang <plause@gmail.com>
2015-01-30 00:12:40 +0800

    Create initial Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  require('load-grunt-tasks')(grunt);
+
+  var config = {};
+
+  grunt.initConfig(config);
+
+  grunt.registerTask('default', []);
+};
